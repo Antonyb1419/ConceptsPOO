f84d33efd1720dd0135047d8c1cfb4ad0641fa5c
ANTONY <ANTONY@LAPTOP-DVEBBG9B>
2022-09-13 11:21:14 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO CONCEPTS");
+Console.WriteLine("============");
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 5));
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message);
+}
