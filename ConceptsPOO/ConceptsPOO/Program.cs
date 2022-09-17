b11ca1bed18e1eeec81714a55d5a36e13e4d60ec
ANTONY <ANTONY@LAPTOP-DVEBBG9B>
2022-09-16 19:13:56 -0500

    Employee, SalaryEmployee & CommissionEmployee finish

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -3,14 +3,31 @@ using ConceptsPOO;
 
 Console.WriteLine("POO CONCEPTS");
 Console.WriteLine("============");
-try
-{
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(1974, 9, 23));
-	Console.WriteLine(new Date(1985, 11, 5));
-}
-catch (Exception ex)
-{
 
-	Console.WriteLine(ex.Message);
-}
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.43M
+};
+
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 15),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+    
+};
+
+Console.WriteLine(employee2);
