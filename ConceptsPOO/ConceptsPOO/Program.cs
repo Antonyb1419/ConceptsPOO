8f19b567747b4be2063556142c988bee744346dc
ANTONY <ANTONY@LAPTOP-DVEBBG9B>
2022-09-21 00:27:43 -0500

    finish Concepts POO

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -15,7 +15,7 @@ Employee employee1 = new SalaryEmployee()
     Salary = 1815453.43M
 };
 
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
 
 Employee employee2 = new CommissionEmployee()
 {
@@ -30,4 +30,73 @@ Employee employee2 = new CommissionEmployee()
     
 };
 
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
+
+Employee employee3 = new HourlyEmployee()
+{
+    Id = 3030,
+    FirstName = "Gonzalo",
+    LastName = "Cardona",
+    BirthDate = new Date(1985, 5, 23),
+    HiringDate = new Date(2022, 2, 15),
+    IsActive = true,
+    HourValue = 12356.56M,
+    Hours = 123.5F
+
+};
+
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommissionEmployee()
+{
+    Id = 4040,
+    FirstName = "Jazmin",
+    LastName = "Salazar",
+    BirthDate = new Date(1988, 5, 23),
+    HiringDate = new Date(2022, 2, 15),
+    IsActive = true,
+    Base = 868678.45M,
+    Sales = 58000000M,
+    CommissionPercentaje = 0.015F
+
+};
+
+//Console.WriteLine(employee4);
+
+ICollection<Employee> employees = new List<Employee>()
+{
+    employee1,
+    employee2,
+    employee3,
+    employee4
+};
+
+decimal payroll = 0;
+
+foreach (Employee employee in employees)
+{
+    Console.WriteLine(employee);
+    payroll+= employee.GetValueToPay();
+}
+
+Console.WriteLine("                                  ===============");
+Console.WriteLine($"TOTAL                          {$"{payroll:C2}",18}");
+
+Invoice invoice1 = new Invoice()
+{
+    Description = "iphone 13",
+    Id = 1,
+    Price = 5300000M,
+    Quantity = 6
+};
+
+Invoice invoice2 = new Invoice()
+{
+    Description = "Posta Preum",
+    Id = 2,
+    Price = 32000M,
+    Quantity = 17.5F
+};
+
+Console.WriteLine(invoice1);
+Console.WriteLine(invoice2);
